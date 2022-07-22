aceebd9112460d3ee6c17063266458486ae74344
Mason-GR <mase210@gmail.com>
2022-07-21 23:27:38 -0400

    added win state pt1

--- app.js
+++ app.js
@@ -1,31 +1,66 @@
 const X_CLASS = "x";
 const CIRCLE_CLASS = "circle";
+const WIN_COMBOS = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+];
 const tileElements = document.querySelectorAll("[data-tile]");
 const gameGrd = document.getElementById("gameGrd");
 let circlesTurn;
 
-tileElements.forEach((tile) => {
-  tile.addEventListener("click", handleClick, { once: true });
-});
+gameStart();
 
-function handleClick(e) {
-  const cell = e.target;
-  const currentClass = circlesTurn ? CIRCLE_CLASS : X_CLASS;
-  //placeMark
-  placeMark(cell, currentClass);
-  // Check For Winner
-  // Check for Draw
-  // Switch Turn
-  swapTurns();
+function gameStart() {
+  circlesTurn = false;
+  tileElements.forEach((tile) => {
+    tile.addEventListener("click", handleClick, { once: true });
+  });
   setGameGrdHoverClass();
 }
 
-function placeMark(cell, currentClass) {
-  cell.classList.add(currentClass);
+function handleClick(e) {
+  const tile = e.target;
+  const currentClass = circlesTurn ? CIRCLE_CLASS : X_CLASS;
+  //placeMark
+  placeInput(tile, currentClass);
+  // Check For Winner
+  if (isWinner(currentClass)) {
+    endGame(false);
+  }
+  // Check for Draw
+  // Switch Turn
+  switchTurns();
+  setGameGrdHoverClass();
 }
 
-function swapTurns() {
+function placeInput(tile, currentClass) {
+  tile.classList.add(currentClass);
+}
+
+function switchTurns() {
   circlesTurn = !circlesTurn;
 }
 
-function setGameGrdHoverClass() {}
+function setGameGrdHoverClass() {
+  gameGrd.classList.remove(X_CLASS);
+  gameGrd.classList.remove(CIRCLE_CLASS);
+  if (circlesTurn) {
+    gameGrd.classList.add(CIRCLE_CLASS);
+  } else {
+    gameGrd.classList.add(X_CLASS);
+  }
+}
+
+function isWinner(currentClass) {
+  return WIN_COMBOS.some((combination) => {
+    return combination.every((index) => {
+      return tileElements[index].classList.contains(currentClass);
+    });
+  });
+}
